59fe63712433e2f506069b119d6307350663d45b
Diegoalza <80487322+Diegoalza@users.noreply.github.com>
2021-10-21 22:16:17 -0500

    Add files via upload

---
+++ Glactic/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Glactic.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Glactic
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new Views.GlacticPage();//
+            MainPage = new NavigationPage(new GlacticPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Glactic/Models/Pedido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glactic.Models
+{
+    public class Pedido
+    {
+        public string Image { get; set; }
+
+        public string Title { get; set; }
+
+        public string Cont { get; set; }
+
+        public string Price { get; set; }
+    }
+}
